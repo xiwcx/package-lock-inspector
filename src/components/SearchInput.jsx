d3cb7ae38b5f8b0d261b9a9baaa881aa5746cb71
Welch Canavan <3486726-welchcanavan@users.noreply.gitlab.com>
2019-10-27 21:11:23 -0400

    debounce push vs. replace location history

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDebounce } from 'react-use';
 import { useHistory } from 'react-router';
 import useSearchString from '../hooks/search-string';
 
@@ -7,8 +8,21 @@ const getQueryString = (value) => (value ? `?query=${value}` : '');
 function SearchInput() {
     const history = useHistory();
     const searchString = useSearchString();
+    const [settled, setSettled] = useState(true);
+    const handleChange = (event) => (settled
+        ? history.push(getQueryString(event.target.value))
+        : history.replace(getQueryString(event.target.value)));
 
-    const handleChange = (event) => history.push(getQueryString(event.target.value));
+    useEffect(
+        () => setSettled(false),
+        [searchString],
+    );
+
+    useDebounce(
+        () => setSettled(true),
+        1000,
+        [searchString],
+    );
 
     return (
         <label className="search layout-controls__search" htmlFor="search-input">
