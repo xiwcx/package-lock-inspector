4f845b54b0d5de572692f9a44418659043ba7033
I. Welch Canavan <3486726-welchcanavan@users.noreply.gitlab.com>
2019-10-28 23:13:04 +0000

    Inputs

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,20 +1,32 @@
+import classNames from 'classnames';
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { useMeasure } from 'react-use';
 import SearchInput from './SearchInput';
 
 function Inputs({ setPackages }) {
     const clearPackages = () => setPackages({});
+    const [ref, { width }] = useMeasure();
+    const inputsContainerClasses = classNames({
+        inputs: true,
+        'inputs--md': width >= 800,
+    });
 
     return (
-        <div className="layout-controls">
+        <div className={inputsContainerClasses} ref={ref}>
             <SearchInput />
 
+            <Link className="inputs__button inputs__clear-search" to="/">
+                Clear search
+            </Link>
+
             <button
-                className="clear-button"
+                className="inputs__button inputs__clear-packages"
                 onClick={clearPackages}
                 type="button"
             >
-                Clear
+                Clear packages
             </button>
         </div>
     );
